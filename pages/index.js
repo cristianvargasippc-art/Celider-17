ad8ed91fc8f3dde8b7d84c727f40fd3817d0fd99
cristianvargasippc-art <cristianvargasippc@gmail.com>
2026-05-17 12:15:47 -0400

    commit file

--- pages/index.js
+++ pages/index.js
@@ -660,10 +660,8 @@ export default function Home() {
             {/* Documentos */}
             <SectionHeader title="Documentos Requeridos" />
             <div className="form-group">
-              <label className="form-label">Cédula o acta de nacimiento (PDF) *</label>
-              <input type="file" className="form-input"
-                onChange={e => setPdfFile('cedula', e.target.files?.[0])} />
-              <p className="form-hint">{regState.cedula_file || 'Selecciona el PDF. Puede tener cualquier nombre y se subirá al enviar el registro.'}</p>
+              <label className="form-label">Cédula o acta de nacimiento*</label>
+              <input type="number" className="form-input" placeholder="Ej: 402-0991385-1" value={regState.Cédula} onChange={e => setReg('Cédula', e.target.value)} /> maxLength={11}
             </div>
             <div className="form-group">
               <label className="form-label">Fotografía 2x2 (PDF) *</label>
